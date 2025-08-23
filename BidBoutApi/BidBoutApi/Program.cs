c4955993385554cc6e59f1742aa62c36ceb7a6c2
Maksym Poliukhovych <maxpolyuhovich@gmail.com>
2025-08-23 20:11:24 +0300

    Initial commit

---
+++ BidBoutApi/BidBoutApi/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowGithubIO",
+        policy =>
+        {
+            policy.WithOrigins("https://marshmalllows.github.io").AllowAnyMethod().AllowAnyHeader();
+        });
+}); 
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowGithubIO");
+
+app.MapControllers();
+app.Run();
